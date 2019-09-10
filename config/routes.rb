fd4bcb7982676039fa91df134583f7269c12c026
imagiri <keitaro1604@gmail.com>
2019-09-10 13:39:27 +0900

    users_controller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
+  if user_signed_in?
   root to: "diarys#index"
+  else
+  root to: "diarys#top"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
