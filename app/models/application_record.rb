595f27a206c6a82626ad799bd18651dc115a92d2
imagiri <keitaro1604@gmail.com>
2019-09-17 13:56:03 +0900

    フォロー機能実装

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,15 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
