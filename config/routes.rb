e2759514f68bb435350713ea153283432c1a7cab
imagiri <keitaro1604@gmail.com>
2019-09-18 16:05:02 +0900

    お気に入り機能非同期更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'relationships/create'
-  get 'relationships/destroy'
   devise_for :users
   devise_scope :user do
   authenticated :user do
@@ -16,10 +14,10 @@ end
   resources :diaries do
     resources :comments, only:[:create,:index]
   end
+  resources :favorites, only:[:create,:destroy]
 
   resources :tags, only:[:new,:create,:destroy]
 
   resources :relationships, only: [:create, :destroy]
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
