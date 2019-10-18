34da82e3997543704f3d5271644de33db102e365
imagiri <keitaro1604@gmail.com>
2019-10-18 15:46:43 +0900

    モデルバリデーション追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,6 @@
 class Tag < ApplicationRecord
-  validates :tag_name, presence: true, length:{maximum: 50}
+  validates :tag_name, presence: true, length:{maximum: 20}
   has_many :taggings, dependent: :destroy
-  # has_many :diaries, through: :taggings, source: :diary
   has_many :diaries, through: :taggings
 
 end
