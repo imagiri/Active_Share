e7c2f5a1dc9121106e6c3b12c99dec71784bcda0
imagiri <keitaro1604@gmail.com>
2019-10-18 15:46:43 +0900

    モデルバリデーション追加

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,4 @@
 class Image < ApplicationRecord
   belongs_to :diary, optional: true
-
-  mount_uploader :main_image, ImageUploader
-  mount_uploader :sub_image, ImageUploader
-
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,16 @@ class User < ApplicationRecord
   user
   end
 
+
+  def set_reset_password_token
+    raw, enc = Devise.token_generator.generate(self.class, :reset_password_token)
+
+    self.reset_password_token   = enc
+    self.reset_password_sent_at = Time.now.utc
+    save(validate: false)
+    raw
+  end
+
   def self.create_unique_string
   SecureRandom.uuid
   end
