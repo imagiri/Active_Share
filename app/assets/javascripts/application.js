c67d1fae07f7d1849514f2f77c82bf083dec1867
imagiri <keitaro1604@gmail.com>
2019-10-25 11:46:39 +0900

    js読み込み調整

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
 //= require jquery/dist/jquery.js
 //= require bootstrap/dist/js/bootstrap.min
+//= require rails-ujs
+//= require activestorage
 //= require diaries.coffee
 //= require diaries.js
 //= require users.js
